f8eae436c6044e09e44ec27a5fac0ba675ea327a
TobiLight <jgetitdonefast@gmail.com>
2023-10-30 16:54:29 +0100

    Created a new view for the link between Place objects and Amenity objects

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -59,34 +59,35 @@ def delete_place_amenity(place_id, amenity_id):
     storage.save()
     return jsonify({}), 200
 
+
 @app_views.route("/places/<place_id>/amenities/<amenity_id>",
                  methods=["POST"], strict_slashes=False)
 def link_place_amenity(place_id, amenity_id):
     """
     Links an Amenity object to a Place
-    
+
     Args:
         place_id (string): Place object ID
         amenity_id (string): Amenity object ID
-    
+
     Returns:
         The Amenity with the status code 201
     """
     from models.place import Place
     place = storage.get(Place, place_id)
-    
+
     if place is None:
         abort(404)
-        
+
     from models.amenity import Amenity
     amenity = storage.get(Amenity, amenity_id)
-    
+
     if amenity is None:
         abort(404)
-        
+
     if amenity in place.amenities:
         return jsonify(amenity.to_dict()), 200
-    
+
     place.amenities.append(amenity)
-    storage.save()    
+    storage.save()
     return jsonify(amenity.to_dict()), 201
